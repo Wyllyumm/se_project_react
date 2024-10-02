50cf43051d82297288ec2e599ee90c93bda092e7
Wyllyumm <walmozard@hotmail.com>
2024-10-02 11:39:24 -0400

    npm issues

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Header from "../Header/Header";
 function App() {
-  return <div className="page">Hello</div>;
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <Header className="header">
+      <img className="header__logo" />
+      <p className="header__date-and-location">Date, Location</p>
+      <button className="header__add-clothes-btn">+ Add Clothes</button>
+    </Header>
+  );
+}
+
+export default Header;
