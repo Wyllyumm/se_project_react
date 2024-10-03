44dc43ac837ee9454eb78b7dc7ba8b69328a4065
Wyllyumm <walmozard@hotmail.com>
2024-10-03 17:15:40 -0400

    1st submit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">Developed by Will Almozard</p>
+      <p className="footer__text">2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
